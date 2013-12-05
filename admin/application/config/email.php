9dadc18128506ff6191a6ec5cb03128cbb692cf6
dandycp <danlott@hotmail.co.uk>
2013-12-05 07:29:59 +0000

    Added default mail sending configuration

---
+++ admin/application/config/email.php
@@ -0,0 +1,8 @@
+<?php
+
+// default configuration for sending emails
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'mail.clockinpoint.com';
+$config['smtp_user'] = 'no-reply@clockinpoint.com';
+$config['smtp_pass'] = 'just4Send1ng!';
+$config['smtp_port'] = '587';
